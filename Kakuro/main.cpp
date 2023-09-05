176d6246fe989fb58c10f0a9d54d5f9f0cc945d0
RRValue <rrvalue@codeschnipsel.org>
2023-09-05 22:48:02 +0200

    finished first version of possibleValues for sum function

--- Kakuro/main.cpp
+++ Kakuro/main.cpp
@@ -2,29 +2,49 @@
 
 #include <QtCore/QTimer>
 
+#include <iostream>
 #include <set>
+#include <string>
 #include <vector>
 
 using Value = unsigned int;
 using ValuetSet = std::set<Value>;
 using ValueVector = std::vector<Value>;
 
-ValuetSet getPossibleValues(const Value& product, const unsigned int& cellCount)
+ValuetSet getPossibleValuesForSum(const Value& summ, const unsigned int& cellCount)
 {
     if(cellCount == 0 || cellCount > 9)
         return {};
 
-    auto pos_vector = ValueVector(cellCount);
-    std::iota(std::begin(pos_vector), std::end(pos_vector), 1);
+    auto values = ValueVector(9);
+    std::iota(std::begin(values), std::end(values), 1);
 
-    auto s = std::accumulate(std::begin(pos_vector), std::end(pos_vector), 0);
+    auto perm = std::vector<Value>(9, Value(0));
+    std::fill_n(std::begin(perm), cellCount, Value(1));
 
-    return {};
+    auto result = ValuetSet();
+
+    do
+    {
+        auto perm_value = std::vector<Value>(9, Value(0));
+        for(unsigned int i = 0; i < 9; i++)
+            perm_value[i] = values[i] * perm[i];
+
+        perm_value.erase(std::remove(std::begin(perm_value), std::end(perm_value), 0), std::end(perm_value));
+
+        const auto perm_result = std::accumulate(std::begin(perm_value), std::end(perm_value), 0);
+
+        if(perm_result == summ)
+            result.insert(std::cbegin(perm_value), std::cend(perm_value));
+
+    } while(std::prev_permutation(std::begin(perm), std::end(perm)));
+
+    return result;
 }
 
 int main(int argc, char** argv)
 {
-    getPossibleValues(12, 4);
+    const auto pos = getPossibleValuesForSum(12, 4);
 
     Application app(argc, argv);
 
